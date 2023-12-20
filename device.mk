11a552f724598259da3a2b63bd9f8d6bc7a5470d
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2023-12-20 12:05:24 +0530

    build display libs

--- device.mk
+++ device.mk
@@ -49,6 +49,17 @@ PRODUCT_PACKAGES += \
     bootctrl.xiaomi_sm8250.recovery \
     android.hardware.boot@1.1-impl-qti.recovery
 
+#Display	
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/qcom/opensource/commonsys-intf/display
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so \
+    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so \
+
+
 # SHIPPING API
 PRODUCT_SHIPPING_API_LEVEL := 30
 
